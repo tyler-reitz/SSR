267f238b5e734a234344708ef57bb829b83703f6
Tyler Reitz <tdreitz@gmail.com>
2018-01-01 19:03:37 -0800

    Configure browser store

--- server/src/client/index.js
+++ server/src/client/index.js
@@ -1,10 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router } from 'react-router-dom'
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension'
 import Routes from './Routes'
 
+const reducers = (state = {}, action) => state
+
+const store = createStore( reducers, {}, composeWithDevTools(
+    applyMiddleware(thunk)
+))
+
 ReactDOM.hydrate(
-  <Router>
-    <Routes />
-  </Router>,
+  <Provider store={store}>
+    <Router>
+      <Routes />
+    </Router> 
+  </Provider>,
   document.querySelector('#root'))
