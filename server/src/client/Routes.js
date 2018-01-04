1ccf69dd8a8617f875d00e1093055cd5e895220b
Tyler Reitz <tdreitz@gmail.com>
2018-01-03 19:53:43 -0800

    Switch to route configs

--- server/src/client/Routes.js
+++ server/src/client/Routes.js
@@ -1,12 +1,16 @@
 import React from 'react'
-import { Route } from 'react-router-dom'
 
 import Home from './components/Home'
 import Users from './components/Users'
 
-export default () => (
-  <div>
-    <Route exact path="/" component={Home} />
-    <Route path="/users" component={Users} />
-  </div>
-)
+export default [
+  {
+    path: '/',
+    component: Home,
+    exact: true,
+  },
+  {
+    path: '/users',
+    component: Users,
+  },
+]
--- server/src/client/index.js
+++ server/src/client/index.js
@@ -2,6 +2,7 @@ import 'babel-polyfill'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router } from 'react-router-dom'
+import { renderRoutes } from 'react-router-config'
 import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
@@ -17,7 +18,7 @@ const store = createStore( reducers, {}, composeWithDevTools(
 ReactDOM.hydrate(
   <Provider store={store}>
     <Router>
-      <Routes />
+      <div>{renderRoutes(Routes)}</div>
     </Router> 
   </Provider>,
   document.querySelector('#root'))
